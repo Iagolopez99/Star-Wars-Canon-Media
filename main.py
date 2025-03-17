137476c0dad7f738f991f489af4f988259729f77
Iago Lopez <iago@ciencias.unam.mx>
2025-03-16 22:22:09 -0600

    Main, list and profile pages added

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Star Wars Canon Media App",
+    page_icon="✨",
+)
+
+st.title("Main Page")
---
+++ pages/media_list.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Media Table",
+    page_icon="📚",
+)
+
+st.title("Canon Media Table")
