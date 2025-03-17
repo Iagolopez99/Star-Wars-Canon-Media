72c5feb4edbc1462588f51fd53ae0066fd31ae1f
Iago Lopez <iago@ciencias.unam.mx>
2025-03-17 00:23:08 -0600

    Minor changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import subprocess
 
-subprocess.run(["python", "app/scraper.py"])
+#subprocess.run(["python", "app/scraper.py"])
 
 st.set_page_config(
     page_title="Star Wars Canon Media App",
